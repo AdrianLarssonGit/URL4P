d321aef50f2a52f080e8b08740e69ef6b7e558bd
Adrian <developmentgitemail@protonmail.com>
2021-06-28 08:32:56 +0200

    Started clean up of left over code as well as refactoring.

--- src/URL4P.java
+++ src/URL4P.java
@@ -1,14 +1,8 @@
 import java.io.IOException;
 import java.io.*;
-
-import java.net.Socket;
 import java.util.*;
 import java.io.File;
 
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer;
-import java.net.InetSocketAddress;
 
 
 public class URL4P {
